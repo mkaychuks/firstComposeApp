bcf27c98b0d581a61182c808f48849a4fd029802
mkaychuks <sachufusi@gmail.com>
2022-03-08 16:57:33 +0100

    After the Pomodro technique break, practiced constraintSet

--- app/src/main/java/com/example/myfirstcompose/Practice.kt
+++ app/src/main/java/com/example/myfirstcompose/Practice.kt
@@ -13,12 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 
 @Composable
@@ -31,97 +34,121 @@ fun Greeting(){
             .padding(top = 150.dp, bottom = 150.dp, start = 16.dp, end = 16.dp)
             .fillMaxSize()
     ) {
-        // the root CONSTRAINT-LAYOUT starts here
-        ConstraintLayout{
+        BoxWithConstraints {
+            val constraints = portraitModeConstraint(margin = 16.dp)
 
-            val (image, nameText, occupationText, rowStats, buttonCoffee, buttonFollow) = createRefs()
-            val guideLine = createGuidelineFromTop(30.dp)
+            // the root CONSTRAINT-LAYOUT starts here
+            ConstraintLayout(constraints){
+                Image(painter = painterResource(id = R.drawable.ifeanyi),
+                    contentDescription = "Ifeanyi",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .border(width = 5.dp, shape = CircleShape, color = Color.Red)
+                        .layoutId("image")
+                ) // profile image
+                Text(text = "Achufusi Ifeanyi",
+                    fontSize = 13.sp,
+                    modifier = Modifier.layoutId("nameText")
+                ) // profile name
+                Text(text = "Android Developer",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.layoutId("occupationText")
+                ) // Profile occupation
 
-
-
-            Image(painter = painterResource(id = R.drawable.ifeanyi),
-                contentDescription = "Ifeanyi",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .border(width = 5.dp, shape = CircleShape, color = Color.Red)
-                    .constrainAs(image){
-                        top.linkTo(guideLine)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-            ) // profile image
-            Text(text = "Achufusi Ifeanyi",
-                fontSize = 13.sp,
-                modifier = Modifier.constrainAs(nameText){
-                    top.linkTo(image.bottom, margin = 8.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-            ) // profile name
-            Text(text = "Android Developer",
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.constrainAs(occupationText){
-                    top.linkTo(nameText.bottom)
-                    start.linkTo(nameText.start)
-                    end.linkTo(nameText.end)
-                }
-            ) // Profile occupation
-
-            // row that contains the following, followers, posts etc. BEGIN
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .constrainAs(rowStats){
-                        top.linkTo(occupationText.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-            ) {
-                // column for following, followers, posts
-                Column() {
-                    Text(text = "150")
-                    Text(text = "Following", fontWeight = FontWeight.Bold)
-                }
-                Column() {
-                    Text(text = "1500")
-                    Text(text = "Following", fontWeight = FontWeight.Bold)
-                }
-                Column() {
-                    Text(text = "3500")
-                    Text(text = "Tweets", fontWeight = FontWeight.Bold)
-                }
-            } // row that contains the following, followers, posts etc. END
-
-            // Row that houses the buttons START
-
-            Button(onClick = { /*TODO*/ },
-                    modifier = Modifier.constrainAs(buttonFollow){
-                        start.linkTo(parent.start)
-                        end.linkTo(buttonCoffee.start)
-                        top.linkTo(rowStats.bottom)
-                        width = Dimension.wrapContent
-                    }
+                // row that contains the following, followers, posts etc. BEGIN
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .layoutId("rowStats")
                 ) {
-                Text(text = "Follow")
-            }
-            Button(onClick = { /*TODO*/ },
-                    modifier = Modifier.constrainAs(buttonCoffee){
-                        start.linkTo(buttonFollow.end)
-                        end.linkTo(parent.end)
-                        top.linkTo(rowStats.bottom)
-                        width = Dimension.wrapContent
+                    // column for following, followers, posts
+                    Column() {
+                        Text(text = "150")
+                        Text(text = "Following", fontWeight = FontWeight.Bold)
                     }
-                ) {
-                Text(text = "Buy a Cup of Coffee")
-            }
+                    Column() {
+                        Text(text = "1500")
+                        Text(text = "Following", fontWeight = FontWeight.Bold)
+                    }
+                    Column() {
+                        Text(text = "3500")
+                        Text(text = "Tweets", fontWeight = FontWeight.Bold)
+                    }
+                } // row that contains the following, followers, posts etc. END
 
-        } // the root column ends here
+                // Row that houses the buttons START
+
+                Button(onClick = { /*TODO*/ },
+                    modifier = Modifier.layoutId("buttonFollow")
+                ) {
+                    Text(text = "Follow")
+                }
+                Button(onClick = { /*TODO*/ },
+                    modifier = Modifier.layoutId("buttonCoffee")
+                ) {
+                    Text(text = "Buy a Cup of Coffee")
+                }
+
+            } // the root column ends here
+        }
 
     }  // card ends here
+}
+
+
+// constraint set for portrait mode
+private fun portraitModeConstraint(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val image = createRefFor("image")
+        val nameText = createRefFor("nameText")
+        val occupationText = createRefFor("occupationText")
+        val rowStats = createRefFor("rowStats")
+        val buttonFollow = createRefFor("buttonFollow")
+        val buttonCoffee = createRefFor("buttonCoffee")
+        val guideLine = createGuidelineFromTop(0.1f)
+
+        constrain(image){
+            top.linkTo(guideLine)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(nameText){
+            top.linkTo(image.bottom, margin = 8.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(occupationText){
+            top.linkTo(nameText.bottom)
+            start.linkTo(nameText.start)
+            end.linkTo(nameText.end)
+        }
+
+        constrain(rowStats){
+            top.linkTo(occupationText.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(buttonFollow){
+            start.linkTo(parent.start)
+            end.linkTo(buttonCoffee.start)
+            top.linkTo(rowStats.bottom)
+            width = Dimension.wrapContent
+        }
+
+        constrain(buttonCoffee){
+            start.linkTo(buttonFollow.end)
+            end.linkTo(parent.end)
+            top.linkTo(rowStats.bottom)
+            width = Dimension.wrapContent
+        }
+    }
 }
 
 @Preview(showBackground = true)
