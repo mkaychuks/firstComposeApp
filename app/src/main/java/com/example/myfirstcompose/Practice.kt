c29757d6e183dfefbece1de45b02872926cd1349
mkaychuks <sachufusi@gmail.com>
2022-03-10 11:54:41 +0100

    Learning and unlearning stateless and state

--- app/src/main/java/com/example/myfirstcompose/Practice.kt
+++ app/src/main/java/com/example/myfirstcompose/Practice.kt
@@ -9,7 +9,8 @@ import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -152,8 +153,38 @@ private fun portraitModeConstraint(margin: Dp): ConstraintSet {
     }
 }
 
+
+
+@Composable
+fun statelessManagement(){
+    var nameState by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(text = "Hello")
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(value = nameState, onValueChange = {
+                nameState = it
+        })
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Display")
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewUs(){
-    Greeting()
+    //Greeting()
+    statelessManagement()
 }
